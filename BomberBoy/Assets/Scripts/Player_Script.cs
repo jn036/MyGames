b4340a78034df0c8a8f36ad94b9f5ed2146758e4
jn036 <jrnetto.netto@gmail.com>
2019-12-03 22:43:14 -0300

    Finalizado
    BomberBoy pronto

---
+++ BomberBoy/Assets/Scripts/Player_Script.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Script : MonoBehaviour
+{
+
+    public float speed = 10f;
+    private float minX = -2.59f;
+    private float maxX = 2.59f;
+    void Update()
+    {
+        MovePlayer();
+        
+    }
+
+    void MovePlayer()
+    {
+        float h = Input.GetAxis("Horizontal");
+        Vector2 currentPosition = transform.position;
+        if (h > 0)
+        { //right
+            currentPosition.x += speed * Time.deltaTime;
+        }
+        else if (h < 0)
+        { //left
+            currentPosition.x -= speed * Time.deltaTime;
+        }
+        if (currentPosition.x < minX)
+        {
+            currentPosition.x = minX;
+        }
+        if (currentPosition.x > maxX)
+        {
+            currentPosition.x = maxX;
+        }
+        transform.position = currentPosition;
+    }
+     void OnTriggerEnter2D(Collider2D target)
+    {
+        if (target.tag == "Bomb")
+        {
+            Time.timeScale = 0f;
+        }
+    }
+}
---
+++ BomberBoy/Assets/Scripts/Spawner_Script.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner_Script : MonoBehaviour
+{
+    public GameObject bombPrefab;
+    private float minX = -2.59f;
+    private float maxX = 2.59f;
+    void Start()
+    {
+        StartCoroutine(SpawnBombs());
+    }
+
+    IEnumerator SpawnBombs()
+    {
+        yield return new WaitForSeconds(Random.Range(0f, 1f));
+
+        Instantiate(bombPrefab, new Vector2(Random.Range(minX, maxX), transform.position.y), 
+            Quaternion.identity);
+
+        StartCoroutine(SpawnBombs());
+
+    }
+}
